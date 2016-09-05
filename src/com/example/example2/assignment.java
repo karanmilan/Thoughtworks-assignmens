2c6eca852c4ddd2c8a5645476ec5f019af38ba26
Karan Jain <karanmilan@gmail.com>
2016-09-05 19:17:14 +0530

    Submitting the second excerxise

---
+++ src/com/example/example2/assignment.java
@@ -0,0 +1,12 @@
+package com.example.example2;
+/**
+ * Created by karan on 05-Sep-16.
+ */
+public class assignment {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0; i<n; i++){
+            System.out.print("*");
+        }
+    }
+}
