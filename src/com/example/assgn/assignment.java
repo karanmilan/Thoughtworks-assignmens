7010cf09831c475576ce21d9bcf303ac63bc9954
Karan Jain <karanmilan@gmail.com>
2016-09-05 18:49:24 +0530

    Adding the first assignment excercise to print the asterix '*'

---
+++ src/com/example/assgn/assignment.java
@@ -0,0 +1,14 @@
+package com.example.assgn;
+
+import java.awt.*;
+
+/**
+ * Created by karan on 05-Sep-16.
+ */
+public class assignment {
+    public static void main(String args[])
+    {
+        System.out.println("*");
+    }
+}
+ww
