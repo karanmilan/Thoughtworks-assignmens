9ee0e72c6c4f6ca878e3fb9475c20971ab417d84
Karan Jain <karanmilan@gmail.com>
2016-09-05 19:21:56 +0530

    Adding the third assignment

---
+++ src/com/example/com/example/example3/assignment.java
@@ -0,0 +1,13 @@
+package com.example.com.example.example3;
+
+/**
+ * Created by karan on 05-Sep-16.
+ */
+public class assignment {
+    public static void main(String args[]){
+        int n=7;
+        for(int i=0; i<n; i++){
+            System.out.println("*");
+        }
+    }
+}
